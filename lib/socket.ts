148fa540ce31f2989d1fdd79825e982f49396683
Akshat Chopra <akshatchopra009@gmail.com>
2026-07-27 00:39:30 +0530

    socket

--- lib/socket.ts
+++ lib/socket.ts
@@ -6,7 +6,7 @@ let socket: Socket | null = null;
 
 export function getSocket(accessToken: string | null): Socket {
   if (!socket) {
-    socket = io(process.env.NEXT_PUBLIC_API_URL, {
+    socket = io(process.env.NEXT_PUBLIC_SOCKET_URL, {
       auth: { token: accessToken },
       autoConnect: false,
     });
